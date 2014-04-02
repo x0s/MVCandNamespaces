521f73e92feeca7bef078aaba811e5ff1b27dfe3
x0s <guillaume.fidanza@gmail.com>
2014-04-02 13:04:56 +0200

    MVC Configuration: - Comments added next to Constants definitions in frontal controller index.php - As a matter of readibility, files involved in using constants annotated in comments.

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <?php
 namespace MVCandNamespaces;
 
-//MVC Configuration
-//Constants written in Capital letters (PSR-1(4.1))
+# MVC Configuration
+#	(Constants written in Capital letters (PSR-1(4.1)) 
+#	To improve readability and to lessen tracing issues, files requiring constants are specified in comments.
 
+#	PATH_VIEWS: Path to the directory containing the Views
+#		@Controllers/*.php
 define('PATH_VIEWS','app/views/');
-define('PATH_LIB','lib/');
-define('ROOT', realpath(__DIR__."/..")."\\"); //Path to the Vendor directory MVCandNamespaces
+
+#	PATH_LIB: Path to the MVC brain
+#		@index.php
+define('PATH_LIB','lib/'); 
+
+#	ROOT: Absolute path to the Vendor directory MVCandNamespaces (excluding itself)
+#		@bootstrap.php
+define('ROOT', realpath(__DIR__."/..")."\\");
+
+#	NAMESPACE_CONTROLLERS: Namespace of the controllers. Should match with autoload
+#		@lib/Routeur.php
 define('NAMESPACE_CONTROLLERS',"MVCandNamespaces\app\controllers\\");
 
 use MVCandNamespaces\lib\Kernel;
